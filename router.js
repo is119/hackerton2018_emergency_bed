d6254b4bc50b28fd1df94a07ed2a488497402f40
HardPlant <abc7988se@naver.com>
2018-05-28 01:52:12 -0700

    handles pin
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- router.js
+++ router.js
@@ -1,6 +1,46 @@
+var pin1 = require("pi-pins").connect(121),
+    pin2 = require("pi-pins").connect(122),
+    pin3 = require("pi-pins").connect(123),
+    pin4 = require("pi-pins").connect(124),
+    pin5 = require("pi-pins").connect(125),
+    pin6 = require("pi-pins").connect(126),
+    pin7 = require("pi-pins").connect(127),
+    pin8 = require("pi-pins").connect(129);
+
+pin1.mode('out')
+pin2.mode('out')
+pin3.mode('out')
+pin4.mode('out')
+pin5.mode('in')
+pin6.mode('in')
+pin7.mode('in')
+pin8.mode('in')
+
 exports.init = (server)=>{
+    pin5.on('fall', function(){
+        pin1.mode(!pin1.value())
+    })
+    pin6.on('fall', function(){
+        pin2.mode(!pin2.value())    
+    })
+    pin7.on('fall', function(){
+        pin3.mode(!pin3.value())    
+    })
+    pin8.on('fall', function(){
+        pin4.mode(!pin4.value())    
+    })
+
     server.get('/', function (req, res) {
+        msg = `${pin5.value(),pin6.value(),pin7.value(),pin8.value()}`
         res.send("Hello World!");
     });
     return server
+}
+
+function loop_handler(){
+
+}
+function loop(){
+    loop_handler()
+    setTimeout(loop,1000);
 }
