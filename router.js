20c13e4c2b350451e95a33ae8231837f435dabf8
HardPlant <abc7988se@naver.com>
2018-06-01 05:44:54 -0700

    update
    Signed-off-by: HardPlant <abc7988se@naver.com>

--- router.js
+++ router.js
@@ -25,31 +25,50 @@ let BusinessNetworkConnection = composer.BusinessNetworkConnection;
 let connection = new BusinessNetworkConnection();
 let definition = "";
 let factory = definition.getFactory();
-
+//TODO : BedID에 LED 할당 (1,2,3,4)
+let bed1 = '0';
+let bed2 = '1';
+let bed3 = '2';
+let bed4 = '3';
 businessNetworkConnection.on('event', (event) => {
     // event: { "$class": "org.namespace.BasicEvent", "eventId": "0000-0000-0000-000000#0" }
-    if(event.$class = "org.hackerton.EmergencyBedOccupied"){
-        if() // BedID가 자신의 ID인지 알 필요 있음
+    if(event.$class = "org.hackerton.EmergencyBedOccupied"){ // ID를 확인해 pin 점등
+        if(event.bed == bed1) {}
+        if(event.bed == bed2) {}
+        if(event.bed == bed3) {}
+        if(event.bed == bed4) {}
+    }
+    else if(event.$class = "EmergencyBedOccupiedComplete"){ // Button 트랜잭션 완료되면 pin 소등
+        if(event.bed == bed1) {pin1.value(false)} // off
+        if(event.bed == bed2) {pin2.value(false)}
+        if(event.bed == bed3) {pin3.value(false)}
+        if(event.bed == bed4) {pin4.value(false)}
     }
     console.log(event);
+    
 });
 
 
 function btn1_on(){
     pin1.value(!pin1.value())
     console.log("pin1 pochit")
+    //BedID에 해당하는 bedOccupiedComplete 트랜잭션 보냄
+    connection.submitTransaction()// org.hackerton.bedOccupiedComplete
 }
 function btn2_on(){
     pin2.value(!pin2.value())
     console.log("pin2 pochit")
+    connection.submitTransaction()// org.hackerton.bedOccupiedComplete
 }
 function btn3_on(){
     pin3.value(!pin3.value())
     console.log("pin3 pochit")
+    connection.submitTransaction()// org.hackerton.bedOccupiedComplete
 }
 function btn4_on(){
     pin4.value(!pin4.value())
     console.log("pin4 pochit")
+    connection.submitTransaction()// org.hackerton.bedOccupiedComplete
 }
 
 exports.init = function(server){
